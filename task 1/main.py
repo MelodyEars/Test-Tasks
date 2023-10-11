9c9a1afbae9afa359e42213200042b2e45b80fa0
SimSams <danya221299@gmail.com>
2023-10-11 06:45:47 -0700

    success solve

--- task 1/main.py
+++ task 1/main.py
@@ -1,44 +1,54 @@
-from collections import deque
-
 from loguru import logger
+
+zero = []
+one = []
+two = []
+three = []
+four = []
+five = []
+six = []
+seven = []
+eight = []
+nine = []
 
 
 def shuffle_msg(key_list: list, msg: list) -> list:
-	len_key = len(key_list)
-	chiphered_msg = []
+	key_to_list = {key: [] for key in key_list}
 
-	steep_count = len_key
-	num = len_key
-
-	step_msg = msg[:num]  # ['LL', 'O', 'HE']
+	steep_count = len(key_list)
+	num = steep_count
+	step_msg = msg[:num]  # ['HE', 'L', 'LO', 'W', 'OR', 'L', 'D']
 
 	while step_msg:
-		for count, key in enumerate(key_list):  # key_list = [2, 3, 1]
-			chiphered_msg.insert(key - 1, step_msg[count])
+		for i, key in enumerate(key_list):
+			try:
+				key_to_list[key].append(step_msg[i])
+				print(key_to_list)
+			except IndexError:
+				pass
 
 		resent_num = num
 		num += steep_count
 		step_msg = msg[resent_num:num]
 
+	chiphered_msg = [item for key in key_list for item in key_to_list[key]]
 	print(chiphered_msg)
 	return chiphered_msg
 
 
 def create_list_with_digraph(msg):
 	msg_list = []
-
-	steep_count = 3
+	step_count = 3
 	num = 3
-
 	step_msg = msg[:num]
 
 	while step_msg:
-		msg_list.append(step_msg[0: 2])
+		msg_list.append(step_msg[0:2])
 		if len(step_msg) == 3:
 			msg_list.append(step_msg[2])
 
 		resent_num = num
-		num += steep_count
+		num += step_count
 		step_msg = msg[resent_num:num]
 
 	print(msg_list)
@@ -46,13 +56,15 @@ def create_list_with_digraph(msg):
 
 
 if __name__ == '__main__':
-	# key_list = [2, 3, 1]
-	# msg = "LLOHE"
-	keys = input('Enter the key via space: ').split()
-	msg = input('Enter the message: ')
-	key_list = list(map(int, keys))
+	key_list = [1, 2]
+	msg = "HELLOWORLD"
+
+	# keys = input('Enter the key via space: ').split()
+	# msg = input('Enter the message: ')
+	# key_list = list(map(int, keys))
 
 	list_with_digraph = create_list_with_digraph(msg)
 	chiphered_msg = shuffle_msg(key_list=key_list, msg=list_with_digraph)
 
 	logger.warning("".join(chiphered_msg))
+
