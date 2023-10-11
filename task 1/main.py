e114e59ba67ff28d251e5eedb57cbf9e72d69c3a
SimSams <danya221299@gmail.com>
2023-10-11 07:13:11 -0700

    success solve 2.0

--- task 1/main.py
+++ task 1/main.py
@@ -1,19 +1,8 @@
 from loguru import logger
 
-zero = []
-one = []
-two = []
-three = []
-four = []
-five = []
-six = []
-seven = []
-eight = []
-nine = []
-
 
 def shuffle_msg(key_list: list, msg: list) -> list:
-	key_to_list = {key: [] for key in key_list}
+	key_to_list = {key: [] for key in sorted(key_list)}
 
 	steep_count = len(key_list)
 	num = steep_count
@@ -21,17 +10,16 @@ def shuffle_msg(key_list: list, msg: list) -> list:
 
 	while step_msg:
 		for i, key in enumerate(key_list):
-			try:
+			if i < len(step_msg):
 				key_to_list[key].append(step_msg[i])
-				print(key_to_list)
-			except IndexError:
-				pass
 
 		resent_num = num
 		num += steep_count
 		step_msg = msg[resent_num:num]
 
-	chiphered_msg = [item for key in key_list for item in key_to_list[key]]
+		print(key_to_list)
+
+	chiphered_msg = ''.join([''.join(value) for key, value in sorted(key_to_list.items())])
 	print(chiphered_msg)
 	return chiphered_msg
 
@@ -56,15 +44,13 @@ def create_list_with_digraph(msg):
 
 
 if __name__ == '__main__':
-	key_list = [1, 2]
-	msg = "HELLOWORLD"
-
-	# keys = input('Enter the key via space: ').split()
-	# msg = input('Enter the message: ')
-	# key_list = list(map(int, keys))
+	keys = input('Enter the key via space: ').split()
+	msg = input('Enter the message: ')
+	key_list = list(map(int, keys))
 
 	list_with_digraph = create_list_with_digraph(msg)
 	chiphered_msg = shuffle_msg(key_list=key_list, msg=list_with_digraph)
 
 	logger.warning("".join(chiphered_msg))
+
 
