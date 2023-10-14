faae8153fc273b98e85d7788bb75609c816cda3a
SimSams <danya221299@gmail.com>
2023-10-14 08:18:37 -0700

    solve 1.0

--- task 2/main.py
+++ task 2/main.py
@@ -1,5 +1,7 @@
 
 NEW_ELEMENTS = 3
+DICT_SEQUENCE = {}
+
 
 def reducer(sequence1):
 	sequence2 = []
@@ -10,32 +12,35 @@ def reducer(sequence1):
 	return sequence2
 
 
-def adder(sequence, step):
-	for i in range(NEW_ELEMENTS):
-		sequence.append(sequence[-1] + step)
+def adder():
+	for key in sorted(DICT_SEQUENCE.keys(), reverse=True):
+		if key != 1:
+			next_dict_elem = DICT_SEQUENCE[key - 1]
+			last_num = DICT_SEQUENCE[key][-1]
+			step = last_num + next_dict_elem[-1]
+			next_dict_elem.append(step)
 
-	return sequence
 
-def calculator(msg):
-	dict_sequence = {"1": msg}
+def calculator(sequence: list, resent_key=1):
+	global DICT_SEQUENCE
+	resent_key += 1
+	result_1 = sequence[1] - sequence[0]
+	result_2 = sequence[2] - sequence[1]
 
-	for key, sequence in dict_sequence.items():
-		result_1 = sequence[1] - sequence[0]
-		result_2 = sequence[2] - sequence[1]
+	if result_2 == result_1:
+		DICT_SEQUENCE[resent_key] = [result_1]
+		[adder() for _ in range(NEW_ELEMENTS)]
 
-		if result_2 == result_1:
-			for i in dict_sequence.keys():
-				dict_sequence[i] = dict_sequence[i] + adder(sequence, result_1)
+	else:
+		DICT_SEQUENCE[resent_key] = reducer(sequence)
+		return calculator(DICT_SEQUENCE[resent_key], resent_key)
 
-		else:
-			dict_sequence[int(key) + 1] = reducer(sequence)
-
-	return dict_sequence["1"][-3:]
-
-			# решить рекурсией, глобальная преремен для словаря и рекурсивно функцией вьісщитьівать разницу
+	print(DICT_SEQUENCE)
+	return DICT_SEQUENCE[1][-3:]
 
 
 if __name__ == '__main__':
-	msg = [15, 32, 57, 90, 131, 180]
-	# msg = [12, 14, 16, 18, 20]
-	print(calculator(msg))
+	# input_sequence = [15, 32, 57, 90, 131, 180]
+	input_sequence = input('Enter the sequence via space: ').split()
+	DICT_SEQUENCE = {1: list(map(int, input_sequence))}
+	print(calculator(sequence=DICT_SEQUENCE[1]))
